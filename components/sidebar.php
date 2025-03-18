05953610464a4a2823a819e239e646dddddeab6f
VeilSide2004 <mondalaranya925@gmail.com>
2025-03-18 08:54:23 +0530

    user interface ui updated

--- components/sidebar.php
+++ components/sidebar.php
@@ -4,9 +4,9 @@
     <h2>Menu</h2>
     <ul>
         <li><a href="wishlist.php">Wishlist</a></li>
-        <li><a href="cart.php">🛒 Cart</a></li>
+        <li><a href="cart.php">Cart</a></li>
         <li class="dropdown">
-            <a href="#">❓ Help & Support</a>
+            <a href="#">Help & Support</a>
             <ul class="dropdown-menu">
                 <li><a href="account.php">Your Account</a></li>
                 <li><a href="signup_form.php">Register</a></li>
